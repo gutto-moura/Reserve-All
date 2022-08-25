e2a867ce09a367650cb417fd05c0c98ddb4a1ca0
gutto-moura <gmourah1792@gmail.com>
2022-08-25 09:28:22 -0300

    Refatorando saga

--- src/store/modules/reservas/sagas.js
+++ src/store/modules/reservas/sagas.js
@@ -1,11 +1,35 @@
-import { call, put, all, takeLatest } from "redux-saga/effects";
+import { select, call, put, all, takeLatest } from "redux-saga/effects";
 import api from "../../../services/api";
-import { addReserveSucess } from "./actions";
+import { addReserveSuccess, updateAmountReserve } from "./actions";
 
 function* addToReserve({ id }){
-    const response = yield call(api.get, `trips/${id}`);
+    const tripExists = yield select(
+        state => state.reserve.find(trip => trip.id ===id)
+    );
+    const myStock = yield call(api.get, `/stock/${id}`);
+    const stockAmount = myStock.data.amount;
 
-    yield put(addReserveSucess(response.data))
+    const currentStock = tripExists ? tripExists.amount : 0;
+
+    const amount = currentStock + 1;
+    if(amount > stockAmount){
+        alert('Quantidade de estoque atingido');
+        return;
+    }
+    if(tripExists){
+        const amount = tripExists.amount + 1;
+
+        yield put(updateAmountReserve(id, amount));
+    }else{
+        const response = yield call(api.get, `trips/${id}`);
+        
+        const data = {
+            ...response.data,
+            amount: 1
+        };
+        yield put(addReserveSuccess(data))
+    }
+
 }
 
 export default all ([
